44d7c6549b58f568a45a70d4f3756c17f93b377e
ReaperDeleter <louis.schulle@gmail.com>
2026-06-26 21:55:46 +0200

    currentUptodate

--- todo-list-node/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    host:     process.env.DB_HOST     || 'm183-lb2-db',
-    user:     process.env.DB_USER     || 'root',
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME     || 'm183_lb2'
-};
